9661bffc89c0ddafb8f6cb679a7e3029934a59ee
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-19 10:44:21 +0200

    chore - footer mobile ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,105 +15,110 @@ export const Footer = () => {
   return (
     <div className="mt-10 py-10 flex items-center justify-between bg-[#121112] text-white">
       <Container>
-        <div className="flex items-center justify-between">
-          <div className="self-start">
-            <h1 className={cn(atma.className, "text-4xl")}>AKD KIDS</h1>
-            <p className="my-5 max-w-sm">
-              It is Turkey&apos;s largest chain of pre-school education
-              institutions.
-            </p>
-            <div className="flex items-center gap-5">
-              <Link
-                href={"/"}
-                className="bg-primaryRed rounded-full flex items-center p-2.5"
-              >
-                <TiSocialFacebook className="text-2xl" />
-              </Link>
-              <Link
-                target="_blank"
-                href={"https://www.instagram.com/afyontriovistaakdkids/"}
-                className="bg-primaryRed rounded-full flex items-center p-2.5"
-              >
-                <FaInstagram className="text-2xl" />
-              </Link>
-            </div>
-          </div>
-          <div className="self-start">
-            <h1
-              className={cn(
-                atma.className,
-                "text-4xl",
-                "border-b-2 border-b-primaryRed w-1/2 whitespace-nowrap"
-              )}
-            >
-              Contact Us
-            </h1>
-            <div className="flex items-center gap-2.5 mt-5">
-              <FaPhoneAlt className="text-sm text-primaryRed self-start" />
-              <Link href="tel:0850 80 80 453" className="">
-                <p className="text-lg">Phone</p>
-                <p className="text-sm">0850 80 80 453</p>
-              </Link>
-            </div>
-            <div className="flex items-center gap-2.5 mt-5">
-              <IoIosMail className="text-xl text-primaryRed self-start" />
-              <Link href="mailto:info@akdkids.com" className="">
-                <p className="text-lg">Email</p>
-                <p className="text-sm">info@akdkids.com</p>
-              </Link>
-            </div>
-          </div>{" "}
-          <div className="self-start">
-            <h1
-              className={cn(
-                atma.className,
-                "text-4xl",
-                "border-b-2 border-b-primaryRed w-1/2 whitespace-nowrap"
-              )}
-            >
-              Quick Menu
-            </h1>
-            <div className="flex flex-col gap-2.5 mt-5">
-              {EducationModels.map((educationModel) => (
+        <div className="flex flex-col gap-10 sm:flex-row sm:items-center sm:justify-between">
+          <div className="flex flex-col self-start gap-10 md:flex-row">
+            <div className="self-start">
+              <h1 className={cn(atma.className, "text-4xl")}>AKD KIDS</h1>
+              <p className="my-5 max-w-sm">
+                It is Turkey&apos;s largest chain of pre-school education
+                institutions.
+              </p>
+              <div className="flex items-center gap-5">
                 <Link
-                  key={educationModel.title}
                   href={"/"}
-                  className="hover:text-primaryRed transition-colors flex items-center gap-2.5"
+                  className="bg-primaryRed rounded-full flex items-center p-2.5"
                 >
-                  <LuDot className="text-xl" /> {educationModel.title}
+                  <TiSocialFacebook className="text-2xl" />
                 </Link>
-              ))}
+                <Link
+                  target="_blank"
+                  href={"https://www.instagram.com/afyontriovistaakdkids/"}
+                  className="bg-primaryRed rounded-full flex items-center p-2.5"
+                >
+                  <FaInstagram className="text-2xl" />
+                </Link>
+              </div>
             </div>
+            <div className="self-start">
+              <h1
+                className={cn(
+                  atma.className,
+                  "text-4xl",
+                  "border-b-2 border-b-primaryRed w-1/2 whitespace-nowrap"
+                )}
+              >
+                Contact Us
+              </h1>
+              <div className="flex items-center gap-2.5 mt-5">
+                <FaPhoneAlt className="text-sm text-primaryRed self-start" />
+                <Link href="tel:0850 80 80 453" className="">
+                  <p className="text-lg">Phone</p>
+                  <p className="text-sm">0850 80 80 453</p>
+                </Link>
+              </div>
+              <div className="flex items-center gap-2.5 mt-5">
+                <IoIosMail className="text-xl text-primaryRed self-start" />
+                <Link href="mailto:info@akdkids.com" className="">
+                  <p className="text-lg">Email</p>
+                  <p className="text-sm">info@akdkids.com</p>
+                </Link>
+              </div>
+            </div>{" "}
           </div>
-          <div className="self-start">
-            <h1
-              className={cn(
-                atma.className,
-                "text-4xl",
-                "border-b-2 border-b-primaryRed w-1/2 whitespace-nowrap"
-              )}
-            >
-              Branches
-            </h1>
-            <div className="flex flex-col gap-2.5 mt-5">
-              <Link
-                href={"/branches/1"}
-                className="text-nowrap hover:text-primaryRed transition-colors flex items-center gap-2.5"
+
+          <div className="flex flex-col self-start gap-10 md:flex-row">
+            <div className="self-start">
+              <h1
+                className={cn(
+                  atma.className,
+                  "text-4xl",
+                  "border-b-2 border-b-primaryRed w-1/2 whitespace-nowrap"
+                )}
               >
-                <MdLocationPin /> Mareşal AKD kids
-              </Link>
-              <Link
-                href={"/branches/1"}
-                className="text-nowrap hover:text-primaryRed transition-colors flex items-center gap-2.5"
+                Quick Menu
+              </h1>
+              <div className="flex flex-col gap-2.5 mt-5">
+                {EducationModels.map((educationModel) => (
+                  <Link
+                    key={educationModel.title}
+                    href={"/"}
+                    className="hover:text-primaryRed transition-colors flex items-center gap-2.5"
+                  >
+                    <LuDot className="text-xl" /> {educationModel.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+            <div className="self-start">
+              <h1
+                className={cn(
+                  atma.className,
+                  "text-4xl",
+                  "border-b-2 border-b-primaryRed w-1/2 whitespace-nowrap"
+                )}
               >
-                <MdLocationPin /> Uydukent AKD kids
-              </Link>
-              <Link
-                href={"/branches/1"}
-                className="text-nowrap hover:text-primaryRed transition-colors flex items-center gap-2.5"
-              >
-                <MdLocationPin /> Erhmen AKD kids
-              </Link>
+                Branches
+              </h1>
+              <div className="flex flex-col gap-2.5 mt-5">
+                <Link
+                  href={"/branches/1"}
+                  className="text-nowrap hover:text-primaryRed transition-colors flex items-center gap-2.5"
+                >
+                  <MdLocationPin /> Mareşal AKD kids
+                </Link>
+                <Link
+                  href={"/branches/1"}
+                  className="text-nowrap hover:text-primaryRed transition-colors flex items-center gap-2.5"
+                >
+                  <MdLocationPin /> Uydukent AKD kids
+                </Link>
+                <Link
+                  href={"/branches/1"}
+                  className="text-nowrap hover:text-primaryRed transition-colors flex items-center gap-2.5"
+                >
+                  <MdLocationPin /> Erhmen AKD kids
+                </Link>
+              </div>
             </div>
           </div>
         </div>
