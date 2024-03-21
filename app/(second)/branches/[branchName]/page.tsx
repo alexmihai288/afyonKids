93fe7685a23e8b3acbf078082583c5fe0112fedb
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-03-21 09:08:52 +0200

    chore -branch name images

--- app/(second)/branches/[branchName]/page.tsx
+++ app/(second)/branches/[branchName]/page.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@/components/Container";
 import { cn } from "@/lib/utils";
 import { Atma } from "next/font/google";
+import Image from "next/image";
 import React from "react";
 import { IoIosPin } from "react-icons/io";
 
@@ -19,7 +20,7 @@ const page = ({ params }: { params: { branchName: string } }) => {
         </div>
       </div>
 
-      <div className="flex items-center mt-20">
+      <div className="flex items-center justify-between mt-20">
         <div className="">
           <p className={cn("text-[#2b3c6b] text-4xl", atma.className)}>
             My English
@@ -32,8 +33,30 @@ const page = ({ params }: { params: { branchName: string } }) => {
           >
             My Second Native English
           </p>
+          <div
+            className={cn(
+              "mt-10 text-4xl bg-[#2c4073] text-white p-5 rounded-full",
+              atma.className
+            )}
+          >
+            <span className="text-[#f6d72e]">A</span>
+            <span className="text-[#e6252b]">K</span>
+            <span className="text-[#2195d2]">D</span>{" "}
+            <span className="text-[#a1c643]">K</span>
+            <span className="text-[#f6d72e]">i</span>
+            <span className="text-[#e6252b]">d</span>
+            <span className="text-[#2195d2]">s</span> / Afyon Mareshal
+          </div>
         </div>
+        <Image
+          src="/branch1.png"
+          width={550}
+          height={550}
+          alt="branch-slider"
+        />
       </div>
+
+      <Image src="/curly.svg" alt="curly arrow" width={100} height={100} />
     </Container>
   );
 };
